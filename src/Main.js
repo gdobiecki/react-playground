e98f9a49ab47f69461726bd925c9bc81d3a6a343
grzesiek <g.dobiecki@gmail.com>
2020-03-19 21:50:19 +0100

    use material tabs, change fav icon

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavBar } from './components/NavBar'
+import NavBar from './components/NavBar'
 import './Main.css';
 
 export class Main extends React.Component {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-export class NavBar extends React.Component {
-  render() {
-    const pages = [];
-    for (let i = 1; i < 5; i++) {
-      pages.push(`item_${i}`)
-    }
-    const navLinks = pages.map(page => {
-      return (
-        <a href={'/' + page}>
-          {page}
-        </a>
-      )
-    });
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
 
-    return <nav>{navLinks}</nav>;
-  }
+export default function NavBar() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+    </Paper>
+  );
 }
